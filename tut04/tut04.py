5c1806bb7d3a7b2b3737e1993ef94c2e87a5c08b
Manav Agarwal <manav_2001me38@iitp.ac.in>
2022-10-16 21:37:10 +0530

    added end time data to data library and added headings for time range

--- tut04/tut04.py
+++ tut04/tut04.py
@@ -1,10 +1,7 @@
-
 from datetime import datetime
 start_time = datetime.now()
 
-#Help https://youtu.be/H37f_x4wAC0
-def octant_longest_subsequence_count_with_range():
-###Code
+import pandas as pd
 
 from platform import python_version
 ver = python_version()
@@ -15,15 +12,139 @@ else:
     print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
 
 
-octant_longest_subsequence_count_with_range()
+mod = 5000
+
+df = pd.read_excel("input_octant_longest_subsequence_with_range.xlsx")
+
+# Taking mean of U, V and W
+U_avg = df['U'].mean()
+V_avg = df['V'].mean()
+W_avg = df['W'].mean()
+
+df["U_avg"] = U_avg      #Defined columns
+
+#To print U_avg once, Otherwise would have filled all the cells below with the same value(U_avg)
+df["U_avg"] = df["U_avg"].head(1)
+
+df["V_avg"] = V_avg
+df["V_avg"] = df["V_avg"].head(1)
+
+df["W_avg"] = W_avg
+df["W_avg"] = df["W_avg"].head(1)
+
+U_subt_U_avg = (df['U'] - U_avg)
+V_subt_V_avg = (df['V'] - V_avg)
+W_subt_W_avg = (df['W'] - W_avg)
+
+#Subtracting U_avg from U. Similiarly with the others.
+df["U' = U - U_avg"] = U_subt_U_avg
+df["V' = V - V_avg"] = V_subt_V_avg
+df["W' = W - W_avg"] = W_subt_W_avg    
+
+octant = []
+
+for i in range(len(df)):
+
+    A = df.loc[i, "U' = U - U_avg"]
+    B = df.loc[i, "V' = V - V_avg"]
+    C = df.loc[i, "W' = W - W_avg"]
+
+        # when U is +ve
+    if A>0:
+        #when V is +ve
+        if B>0:
+            if C>0:
+                octant.append(int(1))
+            else:
+                octant.append(int(-1))
+
+        # when V is -ve
+        else:
+            if C>0:
+                octant.append(int(4))
+            else:
+                octant.append(int(-4))
+
+    # when U is -ve
+    else:
+        # when V is +ve
+        if B>0:
+            if C>0:
+                octant.append(int(2))
+            else:
+                octant.append(int(-2))
+
+        # when V is -ve
+        else:
+            if C>0:
+                octant.append(int(3))
+            else:
+                octant.append(int(-3))
+
+df["Octant"] = octant
+
+#octant names, length, frequency and end time of longest subsequence
+data = {
+        "octants" : ["-4", "-3", "-2", "-1", "1", "2", "3", "4"],
+
+        "length_of_longest_s" : {"1"  : 1,"-1" : 1,"2"  : 1,"-2" : 1,"3"  : 1,"-3" : 1,"4"  : 1,"-4" : 1,},
+
+        "freq" : {"1"  : 0,"-1" : 0,"2"  : 0,"-2" : 0,"3"  : 0,"-3" : 0,"4"  : 0,"-4" : 0,},
+
+        "end_time" : {"1"  : [],"-1" : [],"2"  : [],"-2" : [],"3"  : [],"-3" : [],"4"  : [],"-4" : [],},
+        }
+
+def o_l_subsequence_count(): # octant longest subsequence count
+    l_of_string = 1   # length of string
+    for i in range(len(df) - 1):
+
+        if df["Octant"][i] == df["Octant"][i+1]:
+
+            l_of_string+=1
+
+            # update the longest subsequence by comparing existing value and current length
+            data["length_of_longest_s"][str(df["Octant"][i])] = max(data["length_of_longest_s"][str(df["Octant"][i])], l_of_string)
+
+        #elements are different, reset l_of_string
+        else:
+            l_of_string = 1
+
+#frequency of longest subsequence
+    l_of_string = 1
+    for i in range(len(df) - 1):
+        if df["Octant"][i] == df["Octant"][i+1]:
+            l_of_string+=1
+
+            #current length of subsequence is equal to longest, increament the frequency by one
+            if l_of_string == data["length_of_longest_s"][str(df["Octant"][i])]:
+                data["freq"][str(df["Octant"][i])]+=1
+                data["end_time"][str(df["Octant"][i])].append(df["Time"][i+1])
+
+        else:
+            l_of_string = 1
+
+    df.insert(11, "", value="")
+    df.insert(12, "Count", value="")
+    df.insert(13, "Longest Subsequence Length", value="")
+    df.insert(14, "Frequency", value="")
+
+    # inserting values
+    for i in range(8):
+        df.iloc[i,12] = data["octants"][i]
+        df.iloc[i,13] = data["length_of_longest_s"][ data["octants"][i] ]
+        df.iloc[i,14] = data["freq"][ data["octants"][i] ]
 
 
+#time range of longest subsequence
+    df.iloc[9, 12] = "Count"
+    df.iloc[9, 13] = "Longest Subsequence Length"
+    df.iloc[9, 14] = "Frequency"
 
+o_l_subsequence_count()
 
-
-
-
-
-#This shall be the last lines of the code.
+df.to_excel('output_octant_longest_subsequence_with_range.xlsx', index=False)
 end_time = datetime.now()
-print('Duration of Program Execution: {}'.format(end_time - start_time))
+
+print('Program Runtime: {}'.format(end_time - start_time))
+
+#---------finished---------------------------------------------------------------------------------------------------------
