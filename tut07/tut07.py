df7e1fc3ef73fea66e22536b0511cb59c4aef9a4
Manav Agarwal <manav_2001me38@iitp.ac.in>
2022-11-20 21:40:47 +0530

    Editing Output file and adding borders

--- tut07/tut07.py
+++ tut07/tut07.py
@@ -494,6 +494,156 @@ def octant_subsequence(mod, df, file_name):                                     
         print("Error: Cannot write to output file!", e)
 
 
+def octant_analysis(mod):                                                                            # Editing Output file and adding borders
+ 
+	
+    input_list = os.listdir('E:\\GitHub\\2001ME38_2022\\tut07\\input')                               # Listing input files
+
+    for file_name in input_list:
+        if(file_name[-4:] == 'xlsx'):
+            
+            print(file_name)
+            df = oct_rank(mod, file_name)
+            df = octant_transition(mod, df)
+            df = octant_subsequence(mod, df, file_name)
+
+             
+        input_Path = 'E:\\GitHub\\2001ME38_2022\\tut07\\input\\' + file_name                                                                                                  # Input Path    
+        output_Path = 'E:\\GitHub\\2001ME38_2022\\tut07\\output\\' + str(file_name[0:len(file_name)-5]) + '_vel_octant_analysis_mod_' + str(mod) + '.xlsx'                    # Output path
+
+        w_s = xl.load_workbook(input_Path)
+        sheet = w_s.active
+
+		
+        pattern_fill = PatternFill(pattern_type = "solid", fgColor = "FFFF33")                        # Edits files
+        sheet['L1'].value = ""
+        sheet['AG1'].value = ""
+        sheet['AH1'].value = ""
+        sheet['AR1'].value = ""
+
+        tot_r = df.shape[0]
+        tot_c = df.shape[1]
+        rows_total = math.ceil(tot_r/mod)
+        r = 0
+
+        for row in sheet.iter_rows(min_row = 2, min_col = 1, max_row = tot_r + 1, max_col = tot_c):
+            c = 0
+            for cell in row: 
+                cell.value = df.iat[r, c]
+                c = c + 1
+            r = r + 1
+
+        for row in sheet.iter_rows(min_row = 1, min_col = 14, max_row = rows_total + 2, max_col = 32):
+            for cell in row: 
+                if(cell.value == 1):
+                    cell.fill = pattern_fill
+
+        sheet.column_dimensions['M'].width = 15
+        sheet.column_dimensions['N'].width = 15
+        sheet.column_dimensions['W'].width = 15
+        sheet.column_dimensions['X'].width = 15
+        sheet.column_dimensions['Y'].width = 15
+        sheet.column_dimensions['Z'].width = 15
+        sheet.column_dimensions['AA'].width = 15
+        sheet.column_dimensions['AB'].width = 15
+        sheet.column_dimensions['AC'].width = 15
+        sheet.column_dimensions['AD'].width = 25
+        sheet.column_dimensions['AE'].width = 24
+        sheet.column_dimensions['AF'].width = 18
+        sheet.column_dimensions['AT'].width = 24
+
+
+        border_thin = Border(left=Side(border_style='thin',color='00000000'),
+                        right=Side(border_style='thin',color='00000000'),
+                        top=Side(border_style='thin',color='00000000'),
+                        bottom=Side(border_style='thin',color='00000000')
+                        )
+        border_thick = Border(left=Side(border_style='thick',color='00000000'),
+                    right=Side(border_style='thick',color='00000000'),
+                    top=Side(border_style='thick',color='00000000'),
+                    bottom=Side(border_style='thick',color='00000000')
+                    )
+                        
+        col_n, r_loc, c_loc = 19, 1, 14
+
+        for i in range(r_loc, r_loc + rows_total + 2):
+            for j in range(c_loc, c_loc + col_n):
+                sheet.cell(row = i, column = j).border = border_thick
+
+        for i in range(rows_total + 7,rows_total + 16):
+            for j in range(29, 32):
+                sheet.cell(row = i, column = j).border = border_thick
+
+        for row in sheet.iter_rows(min_row = 4, min_col = 36, max_row = 11, max_col = 43):
+            for cell in row:
+                cell.border = border_thick
+        
+        x = 3
+        for n in range(rows_total+1):
+            i = 0
+            for row in sheet.iter_rows(min_row = x, min_col = 35, max_row = x + 8, max_col = 43):
+                for cell in row:
+                    if(cell.value != None):
+                        cell.border = border_thick
+            x = x + 13
+
+        for row in sheet.iter_rows(min_row = 1, min_col = 45, max_row = 9, max_col = 47):
+            for cell in row:
+                cell.border = border_thick
+
+        r_max = 1
+        for row in range(2, tot_r):
+            if str(sheet.cell(row = row, column = 50).value) == "nan":
+                r_max = row
+                break
+        
+        for row in sheet.iter_rows(min_row = 1, min_col = 49, max_row = r_max - 1, max_col = 51):
+            for cell in row:
+                cell.border = border_thick
+        
+        sheet['E1'] = 'U Avg'
+        sheet['F1'] = 'V Avg'
+        sheet['G1'] = 'W Avg'
+        sheet['H1'] = "U' = U - U Avg"
+        sheet['I1'] = "V' = V - V avg"
+        sheet['J1'] = "W' = W - W avg"
+
+        sheet['K1'] = 'Octant'
+        sheet['N1'] = 'Overall Octant Count'
+        sheet['O1'] = '+1'
+        sheet['P1'] = '-1'
+        sheet['Q1'] = '+2'
+        sheet['R1'] = '-2'
+        sheet['S1'] = '+3'
+        sheet['T1'] = '-3'
+        sheet['U1'] = '+4'
+        sheet['V1'] = '-4'
+
+        sheet['W1'] = 'Rank Octant 1'
+        sheet['X1'] = 'Rank Octant -1'
+        sheet['Y1'] = 'Rank Octant 2'
+        sheet['Z1'] = 'Rank Octant -2'
+        sheet['AA1'] = 'Rank Octant 3'
+        sheet['AB1'] = 'Rank Octant -3'
+        sheet['AC1'] = 'Rank Octant 4'
+        sheet['AD1'] = 'Rank Octant -4'
+
+        sheet['AE1'] = 'rank_1 Octant ID'
+        sheet['AF1'] = 'rank_1 Octant Name'
+        sheet['AI1'] = 'Overall Transition Count'
+        sheet['AS1'] = 'Longest Subsquence Length'
+        sheet['AW1'] = 'Longest Subsquence Length with Range'
+        sheet['AX1'] = 'Longest Subsquence Length'
+        sheet['AY1'] = 'Count'
+
+		
+        w_s.save(output_Path)                                             # Saves the output files
+
+
+mod = 5000
+octant_analysis(mod) 
+
+
 ##Read all the excel files in a batch format from the input/ folder. Only xlsx to be allowed
 ##Save all the excel files in a the output/ folder. Only xlsx to be allowed
 ## output filename = input_filename[_octant_analysis_mod_5000].xlsx , ie, append _octant_analysis_mod_5000 to the original filename. 
@@ -507,12 +657,6 @@ if ver == "3.8.10":
 	print("Correct Version Installed")
 else:
 	print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
-
-
-mod=5000
-octant_analysis(mod)
-
-
 
 
 
